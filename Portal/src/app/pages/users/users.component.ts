a4bf84c428217325e733852141d9bcc8e7fe4aab
Anthony Tran <a.tran@student.avans.nl>
2019-03-28 14:26:43 +0100

    Working API with Call

--- Portal/src/app/pages/users/users.component.ts
+++ Portal/src/app/pages/users/users.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+
+import { UsersService } from '../../service/users.service';
 @Component({
   selector: 'app-icons',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
-
+user;
   public copy: string;
-  constructor() { }
+  constructor(private usersService: UsersService) {
+
+  }
+
+
 
   ngOnInit() {
+
+    const userObservable = this.usersService.getAllUsers();
+    userObservable.subscribe((userData: any[]) => {
+      console.log(userData);
+      this.user = userData;
+    });
+
+
   }
 }
