a88031f1287f9cd3f7b9a61e2e0616624dfe4fea
Anthony Tran <a.tran@student.avans.nl>
2019-03-28 10:33:08 +0100

    init

---
+++ Portal/src/app/layouts/auth-layout/auth-layout.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AuthLayoutRoutes } from './auth-layout.routing';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { LoginComponent } from '../../auth/login/login.component';
+import { RegisterComponent } from '../../auth/register/register.component';
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(AuthLayoutRoutes),
+    FormsModule
+    // NgbModule
+  ],
+  declarations: [
+    LoginComponent,
+    RegisterComponent
+  ]
+})
+export class AuthLayoutModule { }
---
+++ Portal/src/app/pages/users/users.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-icons',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.scss']
+})
+export class UsersComponent implements OnInit {
+
+  public copy: string;
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
