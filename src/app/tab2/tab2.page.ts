4f5245909d74654b706ec6b5ce176dc84ebba845
Luuk van Deursen <ltadeurs@avans.nl>
2019-04-11 20:15:29 +0200

    calendar added

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -47,7 +47,27 @@ export class Tab2Page implements OnInit{
   }
 
   addEvent(){
-    
+    let eventCopy = {
+      title: this.event.title,
+      startTime: new Date(this.event.startTime),
+      endTime: new Date(this.event.endTime),
+      allDay: this.event.allDay,
+      desc: this.event.desc
+    }
+
+    if(eventCopy.allDay){
+      let start = eventCopy.startTime;
+      let end = eventCopy.endTime;
+
+      //2019, 
+      eventCopy.startTime = new Date(Date.UTC(start.getUTCFullYear(), start.getUTCMonth(), start.getUTCDate()));
+      eventCopy.endTime = new Date(Date.UTC(start.getUTCFullYear(), start.getUTCMonth(), start.getUTCDate() + 1));
+    }
+
+    this.eventSource.push(eventCopy);
+    this.myCal.loadEvents();
+    this.resetEvent();
+
   }
 
   onEventSelected(){
