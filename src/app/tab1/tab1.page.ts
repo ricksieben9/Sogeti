c0f99fed8e6bf592af839c4f34e609ff1d1b8d9f
Luuk van Deursen <ltadeurs@avans.nl>
2019-04-11 16:13:18 +0200

    Melding details verborgen

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -10,64 +10,87 @@ import { LocalNotifications, ELocalNotificationTriggerUnit } from '@ionic-native
 export class Tab1Page {
   scheduled = [];
 
-constructor(private plt: Platform, private localNotifications: LocalNotifications, 
-    private alertCtrl: AlertController){
-      this.plt.ready().then(() =>{
-        this.localNotifications.on('click').subscribe(res => {
-          console.log('click: ', res);
-          let msg = res.data ? res.data.mydata : '';
-          this.showAlert(res.title, res.text, msg);
-        });
+  constructor(private plt: Platform, private localNotifications: LocalNotifications,
+    private alertCtrl: AlertController) {
+    this.plt.ready().then(() => {
+      this.localNotifications.on('click').subscribe(res => {
+        console.log('click: ', res);
+        let msg = res.data ? res.data.mydata : '';
+        this.showAlert(res.title, res.text, msg);
+      });
 
-        this.localNotifications.on('trigger').subscribe(res => {
-          console.log('trigger: ', res);
-          let msg = res.data ? res.data.mydata : '';
-          this.showAlert(res.title, res.text, msg);
-        });
-      })
-}
-scheduleNotification(){
-  this.localNotifications.schedule({
-    id: 1,
-    title: 'Let op!',
-    text: 'Pieter heeft medicatie nodig',
-    data: { mydata: 'My hidden message is this'},
-    trigger: { in: 5, unit: ELocalNotificationTriggerUnit.SECOND },
-    //foreground: true
-  });
-}
+      this.localNotifications.on('trigger').subscribe(res => {
+        console.log('trigger: ', res);
+        let msg = res.data ? res.data.mydata : '';
+        this.showAlert(res.title, res.text, msg);
+      });
+    })
+  }
+  scheduleNotification() {
+    this.localNotifications.schedule({
+      id: 1,
+      title: 'Melding',
+      text: 'Medicatie melding!',
+      data: 'Pieter heeft medicatie nodig ' + this.getDate(),
+      trigger: { in: 5, unit: ELocalNotificationTriggerUnit.SECOND },
+      foreground: true
+    });
+  }
 
-recurringNotification(){
-  this.localNotifications.schedule({
-    id: 22,
-    title: 'Recurring ',
-    text: 'Heeft Peter zijn medicatie ingenomen?',
-    trigger: { every: ELocalNotificationTriggerUnit.MINUTE }
-  });
-}
+  recurringNotification() {
+    this.localNotifications.schedule({
+      id: 2,
+      title: 'Herrinnering',
+      text: 'Medicatie herinnering!',
+      data: 'Heeft Peter zijn medicatie ingenomen? ' + this.getDate(),
+      trigger: { every: ELocalNotificationTriggerUnit.MINUTE },
+      foreground: true
+    });
+  }
 
-repeatingDaily(){
-  this.localNotifications.schedule({
-    id: 42,
-    title: 'Goede Morgen ',
-    text: 'Tijd om medicatie te geven',
-    trigger: { every: { hour: 9, minute: 25} }
-  });
-}
+  repeatingDaily() {
+    this.localNotifications.schedule({
+      id: 3,
+      title: 'Melding',
+      text: 'Medicatie melding!',
+      data: 'Pieter heeft medicatie nodig ' + this.getDate(),
+      trigger: { every: { hour: 13, minute: 20 } },
+      foreground: true
+    });
+  }
 
-getAll(){
-  this.localNotifications.getAll().then(res => {
-    this.scheduled = res;
-  });
-}
 
-showAlert(header, sub, msg){
-  this.alertCtrl.create({
-    header: header,
-    subHeader: sub,
-    message: msg,
-    buttons: ['Ok']
-  }).then(alert => alert.present());
-}
+  showAlert(header, sub, msg) {
+    this.alertCtrl.create({
+      header: header,
+      subHeader: sub,
+      message: msg,
+      buttons: ['Ok']
+    }).then(alert => alert.present());
+  }
+
+  getDate() {
+
+    var today = new Date();
+    var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+    return date + '-' + time;
+
+  }
+
+  getAll() {
+    this.localNotifications.getAll().then(res => {
+      this.scheduled = res;
+    });
+  }
+
+  ionViewDidEnter() {
+    this.getAll();
+  }
+
+  removeAll() {
+    this.scheduled = [];
+  }
 
 }
