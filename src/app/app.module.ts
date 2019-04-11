a4b5aeac6ea6e17072888099f3d245c01a1a2990
bartkoevoets <yzig_koevoets@hotmail.com>
2019-04-11 16:12:58 +0200

    Added authentication backend and login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AuthModule } from './auth/auth.module';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
