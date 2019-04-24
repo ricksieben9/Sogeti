54d45b4ae5e0d7c902e2b3f352d0876f2498be0a
Luuk van Deursen <ltadeurs@avans.nl>
2019-04-24 14:34:39 +0200

    TSline errors fixed

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,91 +1,90 @@
-import { Component } from '@angular/core';
-import { Platform, AlertController } from '@ionic/angular';
-import { LocalNotifications, ELocalNotificationTriggerUnit } from '@ionic-native/local-notifications/ngx';
+import {Component} from '@angular/core';
+import {Platform, AlertController} from '@ionic/angular';
+import {LocalNotifications, ELocalNotificationTriggerUnit} from '@ionic-native/local-notifications/ngx';
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+    selector: 'app-tab1',
+    templateUrl: 'tab1.page.html',
+    styleUrls: ['tab1.page.scss']
 })
 
 
 export class Tab1Page {
-  history = [];
+    history = [];
 
 
-  constructor(private plt: Platform, private localNotifications: LocalNotifications, private alertCtrl: AlertController) {
-    this.plt.ready().then(() => {
-      this.localNotifications.on('click').subscribe(res => {
-        let msg = res.data ? res.data.mydata : '';
-        this.showAlert(res.title, res.text, msg);
-      });
+    constructor(private plt: Platform, private localNotifications: LocalNotifications, private alertCtrl: AlertController) {
+        this.plt.ready().then(() => {
+            this.localNotifications.on('click').subscribe(res => {
+                const msg = res.data ? res.data.mydata : '';
+                this.showAlert(res.title, res.text, msg);
+            });
+            this.localNotifications.on('trigger').subscribe(res => {
+                const msg = res.data ? res.data.mydata : '';
+                this.showAlert(res.title, res.text, msg);
+            });
+        });
+    }
 
-      this.localNotifications.on('trigger').subscribe(res => {
-        let msg = res.data ? res.data.mydata : '';
-        this.showAlert(res.title, res.text, msg);
-      });
-    })
-  }
-
-  // When tab is opened
-  ionViewDidEnter() {
-    this.loadHistory();
-  }
-
-
-  // Notification content
-  scheduleNotification() {
-    this.localNotifications.schedule({
-      id: 1,
-      title: 'Melding',
-      text: 'Medicatie melding!',
-      data: 'Pieter heeft medicatie nodig ' + this.getDate(),
-      trigger: { in: 5, unit: ELocalNotificationTriggerUnit.SECOND },
-      foreground: true,
-      wakeup: true,
-      priority: 2,
-      vibrate: true,
-      launch: true,
-      silent: false
-    });
-  }
+    // When tab is opened
+    ionViewDidEnter() {
+        this.loadHistory();
+    }
 
 
-  // When notification is clicked
-  showAlert(header, sub, msg) {
-    this.alertCtrl.create({
-      header: header,
-      subHeader: sub,
-      message: msg,
-      buttons: ['Ok'],
-    }).then(alert => alert.present());
-  }
+    // Notification content
+    scheduleNotification() {
+        this.localNotifications.schedule({
+            id: 1,
+            title: 'Melding',
+            text: 'Medicatie melding!',
+            data: 'Pieter heeft medicatie nodig ' + this.getDate(),
+            trigger: {in: 5, unit: ELocalNotificationTriggerUnit.SECOND},
+            foreground: true,
+            wakeup: true,
+            priority: 2,
+            vibrate: true,
+            launch: true,
+            silent: false
+        });
+    }
 
 
-  // Get current date for notification
-  getDate() {
-    const today = new Date();
-    const date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-    const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    return date + '-' + time;
-  }
+    // When notification is clicked
+    showAlert(header, sub, msg) {
+        this.alertCtrl.create({
+            header: header,
+            subHeader: sub,
+            message: msg,
+            buttons: ['Ok'],
+        }).then(alert => alert.present());
+    }
 
 
-  // Load history of notifications
-  loadHistory() {
-    this.localNotifications.getAll().then(res => {
-      res.forEach(notification => {
-        if (this.history.length == 0)
-          this.history.push(notification);
-        else
-          if (this.history[this.history.length - 1].data !== notification.data)
-            this.history.push(notification);
-      });
-    });
-  }
+    // Get current date for notification
+    getDate() {
+        const today = new Date();
+        const date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+        const time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
+        return date + '-' + time;
+    }
+
+
+    // Load history of notifications
+    loadHistory() {
+        this.localNotifications.getAll().then(res => {
+            res.forEach(notification => {
+                if (this.history.length === 0) {
+                    this.history.push(notification);
+                } else if (this.history[this.history.length - 1].data !== notification.data) {
+                    this.history.push(notification);
+                }
+            });
+        });
+    }
 
 // Remove notification history
-  remove() {
-    this.history = [];
-  }
+    remove() {
+        this.history = [];
+    }
 }
--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,179 +1,184 @@
-import { Component, ViewChild, OnInit, Inject, LOCALE_ID } from '@angular/core';
-import { CalendarComponent } from 'ionic2-calendar/calendar';
-import { AlertController } from '@ionic/angular';
-import { formatDate } from '@angular/common';
-import { DateFormatterService } from '../../services/formatter/date-formatter.service';
+import {Component, ViewChild, OnInit, Inject, LOCALE_ID, AfterViewInit} from '@angular/core';
+import {CalendarComponent} from 'ionic2-calendar/calendar';
+import {AlertController} from '@ionic/angular';
+import {formatDate} from '@angular/common';
+import {DateFormatterService} from '../../services/formatter/date-formatter.service';
 
 @Component({
-  selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss'],
+    selector: 'app-tab2',
+    templateUrl: 'tab2.page.html',
+    styleUrls: ['tab2.page.scss'],
 })
-export class Tab2Page implements OnInit {
+export class Tab2Page implements OnInit, AfterViewInit {
 
-  minDate = new Date().toISOString();
-  eventSource = [];
-  viewTitle = '';
-  dateFormatter = this.dateFormat.getDates();
-  currentWeek;
-  currentDay
+    minDate = new Date().toISOString();
+    eventSource = [];
+    viewTitle = '';
+    dateFormatter = this.dateFormat.getDates();
+    currentWeek;
+    currentDay;
+    collapseCard = true;
 
-  event = {
-    //id
-    title: '',
-    desc: '',
-    startTime: '',
-    endTime: '',
-    allDay: false
-  };
+    event = {
+        // id
+        title: '',
+        desc: '',
+        startTime: '',
+        endTime: '',
+        allDay: false
+    };
 
-  calendar = {
-    allDay: false,
-    mode: 'day',
-    currentDate: new Date(),
-    dateFormatter: this.dateFormat.getDates()
-  }
-
-
-
-  @ViewChild(CalendarComponent) myCal: CalendarComponent;
-
-  constructor(private alertCtrl: AlertController, @Inject(LOCALE_ID) private locale: string, private dateFormat: DateFormatterService) { }
+    calendar = {
+        allDay: false,
+        mode: 'day',
+        currentDate: new Date(),
+        dateFormatter: this.dateFormat.getDates()
+    };
 
 
-  //Before agenda has loaded
-  ngOnInit() {
-    const today = new Date;
-    this.currentWeek = this.dateFormatter.getWeekNumber(today.getFullYear(), today.getMonth(), today.getDate())
-  }
+    @ViewChild(CalendarComponent) myCal: CalendarComponent;
 
-
-  //When agenda is done loading
-  ngAfterViewInit() {
-    this.loadTimeIndicator();
-  }
-
-
-  loadTimeIndicator() {
-    const line = document.createElement('div');
-    line.id = 'timeIndicator';
-    document.querySelectorAll("#timeIndicator").forEach(e => e.parentNode.removeChild(e));
-    if (this.calendar.mode == 'day') {
-      const calendarGrid = document.querySelectorAll('.dayview-normal-event-container[ng-reflect-emit-event="false"]');
-      const calendarcell = calendarGrid[0].querySelector(".dayview-normal-event-table > tbody > tr > .calendar-cell");
-      calendarcell.prepend(line);
-    }
-    if (this.calendar.mode == 'week') {
-      const calendarGrid = document.querySelectorAll('.weekview-normal-event-container[ng-reflect-emit-event="false"]');
-      const calendarcell = calendarGrid[0].querySelectorAll(".weekview-normal-event-table > tbody > tr > .calendar-cell");
-      calendarcell[this.calendar.currentDate.getDay() - 1].prepend(line);
+    constructor(private alertCtrl: AlertController, @Inject(LOCALE_ID) private locale: string, private dateFormat: DateFormatterService) {
     }
 
-    const d = new Date();
-    const pos = (d.getHours() * 36.875) + (d.getMinutes() * 0.6145833);
-    line.style.top = pos + 'px';
-    setInterval(function () {
-      const d = new Date();
-      const pos = (d.getHours() * 36.875) + (d.getMinutes() * 0.6145833);
-      line.style.top = pos + 'px';
-    }, 60000);
-  }
+
+    // Before agenda has loaded
+    ngOnInit() {
+        const today = new Date;
+        this.currentWeek = this.dateFormatter.getWeekNumber(today.getFullYear(), today.getMonth(), today.getDate());
+    }
 
 
-  //Change mode to 'day', 'week, or 'month'
-  changeMode(mode) {
-    if (this.calendar.mode == mode)
-      this.today();
-    this.calendar.mode = mode;
-  }
-
-
-  //Previous date
-  back() {
-    const swiper = document.querySelector('.swiper-container')['swiper'];
-    swiper.slidePrev();
-  }
-
-
-  //Next date
-  next() {
-    const swiper = document.querySelector('.swiper-container')['swiper'];
-    swiper.slideNext();
-  }
-
-
-  //Go to today
-  today() {
-    this.calendar.currentDate = new Date();
-  }
-
-
-  //When event is clicked
-  async onEventSelected(event) {
-    let start = formatDate(event.startTime, 'medium', this.locale);
-    const alert = await this.alertCtrl.create({
-      header: "Ontvanger: " + event.title,
-      subHeader: "Medicijn: " + event.desc,
-      message: 'Om: ' + start,
-      buttons: ['OK']
-    });
-    alert.present();
-  }
-
-
-  //Get event time in week and month mode
-  onTimeSelected(ev) {
-    let selected = new Date(ev.selectedTime);
-    this.event.startTime = selected.toISOString();
-    selected.setHours(selected.getHours() + 1);
-    this.event.endTime = (selected.toISOString());
-  }
-
-
-  //When switched to another day or calendar mode
-  onViewTitleChanged(title) {
-    this.loadDayIndicator(title);
-    this.viewTitle = title;
-  }
-
-
-  loadDayIndicator(title) {
-    if (this.calendar.mode == 'week') {
-      const titleParts = title.split(' ');
-      if (titleParts[3] == this.currentWeek)
+    // When agenda is done loading
+    ngAfterViewInit() {
         this.loadTimeIndicator();
     }
-    if (this.calendar.mode == 'day') {
-      if (this.viewTitle.length == 0)
-        this.currentDay = title;
-      if (title == this.currentDay)
-        this.loadTimeIndicator()
+
+
+    loadTimeIndicator() {
+        const line = document.createElement('div');
+        line.id = 'timeIndicator';
+        document.querySelectorAll('#timeIndicator').forEach(e => e.parentNode.removeChild(e));
+        if (this.calendar.mode === 'day') {
+            const calendarGrid = document.querySelectorAll('.dayview-normal-event-container[ng-reflect-emit-event="false"]');
+            const calendarcell = calendarGrid[0].querySelector('.dayview-normal-event-table > tbody > tr > .calendar-cell');
+            calendarcell.prepend(line);
+        }
+        if (this.calendar.mode === 'week') {
+            const calendarGrid = document.querySelectorAll('.weekview-normal-event-container[ng-reflect-emit-event="false"]');
+            const calendarcell = calendarGrid[0].querySelectorAll('.weekview-normal-event-table > tbody > tr > .calendar-cell');
+            calendarcell[this.calendar.currentDate.getDay() - 1].prepend(line);
+        }
+
+        const startDate = new Date();
+        const startPos = (startDate.getHours() * 36.875) + (startDate.getMinutes() * 0.6145833);
+        line.style.top = startPos + 'px';
+        setInterval(function () {
+            const newDate = new Date();
+            const newPos = (newDate.getHours() * 36.875) + (newDate.getMinutes() * 0.6145833);
+            line.style.top = newPos + 'px';
+        }, 60000);
     }
-  }
 
 
-  resetEvent() {
-    this.event = {
-      title: '',
-      desc: '',
-      startTime: new Date().toISOString(),
-      endTime: new Date().toISOString(),
-      allDay: false
-    };
-  }
-
-  
-  addEvent() {
-    let eventCopy = {
-      title: this.event.title,
-      startTime: new Date(this.event.startTime),
-      endTime: new Date(this.event.endTime),
-      desc: this.event.desc
+    // Change mode to 'day', 'week, or 'month'
+    changeMode(mode) {
+        if (this.calendar.mode === mode) {
+            this.today();
+        }
+        this.calendar.mode = mode;
     }
 
-    this.eventSource.push(eventCopy);
-    this.myCal.loadEvents();
-    this.resetEvent();
 
-  }
+    // Previous date
+    back() {
+        const swiper = document.querySelector('.swiper-container')['swiper'];
+        swiper.slidePrev();
+    }
+
+
+    // Next date
+    next() {
+        const swiper = document.querySelector('.swiper-container')['swiper'];
+        swiper.slideNext();
+    }
+
+
+    // Go to today
+    today() {
+        this.calendar.currentDate = new Date();
+    }
+
+
+    // When event is clicked
+    async onEventSelected(event) {
+        const start = formatDate(event.startTime, 'medium', this.locale);
+        const alert = await this.alertCtrl.create({
+            header: 'Ontvanger: ' + event.title,
+            subHeader: 'Medicijn: ' + event.desc,
+            message: 'Om: ' + start,
+            buttons: ['OK']
+        });
+        alert.present();
+    }
+
+
+    // Get event time in week and month mode
+    onTimeSelected(ev) {
+        const selected = new Date(ev.selectedTime);
+        this.event.startTime = selected.toISOString();
+        selected.setHours(selected.getHours() + 1);
+        this.event.endTime = (selected.toISOString());
+    }
+
+
+    // When switched to another day or calendar mode
+    onViewTitleChanged(title) {
+        this.loadDayIndicator(title);
+        this.viewTitle = title;
+    }
+
+
+    loadDayIndicator(title) {
+        if (this.calendar.mode === 'week') {
+            const titleParts = title.split(' ');
+            if (parseInt(titleParts[3], 0) === this.currentWeek) {
+                this.loadTimeIndicator();
+            }
+        }
+        if (this.calendar.mode === 'day') {
+            if (this.viewTitle.length === 0) {
+                this.currentDay = title;
+            }
+            if (title === this.currentDay) {
+                this.loadTimeIndicator();
+            }
+        }
+    }
+
+
+    resetEvent() {
+        this.event = {
+            title: '',
+            desc: '',
+            startTime: new Date().toISOString(),
+            endTime: new Date().toISOString(),
+            allDay: false
+        };
+    }
+
+
+    addEvent() {
+        const eventCopy = {
+            title: this.event.title,
+            startTime: new Date(this.event.startTime),
+            endTime: new Date(this.event.endTime),
+            desc: this.event.desc
+        };
+
+        this.eventSource.push(eventCopy);
+        this.myCal.loadEvents();
+        this.resetEvent();
+    }
 }
+
