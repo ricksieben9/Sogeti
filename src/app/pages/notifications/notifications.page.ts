5ccf45d45b32b60bc7c43ad7fbdbe34f7ccf6f2f
Luuk van Deursen <lta.vandeursen@student.avans.nl>
2019-05-20 20:25:51 +0200

    Changed viewDidEnter changed to viewWillEnter

--- src/app/pages/notifications/notifications.page.ts
+++ src/app/pages/notifications/notifications.page.ts
@@ -24,7 +24,7 @@ export class NotificationsPage {
 
 
     // Notification page is opened
-    ionViewDidEnter() {
+    ionViewWillEnter() {
         this.refresh();
     }
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   // change server to local desktop ip running the api server
-  apiServerAddress: 'http://192.168.146.1:3000'
+  apiServerAddress: 'http://192.168.1.25:3000'
   // apiServerAddress: 'http://localhost:3000'
 };
 
