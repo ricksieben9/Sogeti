d9ad0958ae593dff5b78b175ddd77b7ca8f197f5
Kenny Tu <ktu@avans.nl>
2019-05-20 16:43:20 +0200

    registrate with 2 inputfields done

--- src/app/pages/registerpin/registerpin.page.ts
+++ src/app/pages/registerpin/registerpin.page.ts
@@ -15,17 +15,26 @@ export class RegisterPinPage {
 
   registerPin(form) {
 	let pin: number;
+	let repeat_pin:number;
 	pin = parseInt(form.value.pincode);
-	if (isNaN(pin)) {
+	repeat_pin = parseInt(form.value.repeat_pincode);
+
+	if (isNaN(pin) && isNaN(repeat_pin)) {
 		this.pinErrorMsg = 'U kunt alleen cijfers in uw pincode gebruiken.';
 	} else {
-		if (pin.toString().length < 5 || pin.toString().length > 8) {
-		this.pinErrorMsg = 'Uw pincode mag niet minder dan 5 en niet meer dan 8 cijfers bevatten.';
-		} else {
-		const username = JSON.parse(localStorage.getItem('CURRENT_USER')).username;
-		const pinuser = { username: username, pin: pin };
-		localStorage.setItem('PIN_CODE_USER', JSON.stringify(pinuser));
-		this.router.navigateByUrl('');
+		//check the length of the PIN
+		if ((pin.toString().length < 5 || pin.toString().length > 8) && (repeat_pin.toString().length < 5 || repeat_pin.toString().length > 8)) {
+			this.pinErrorMsg = 'Uw pincode mag niet minder dan 5 en niet meer dan 8 cijfers bevatten.';
+
+		//check if the first pin equals the confirm pin
+		} else if (pin != repeat_pin){
+			this.pinErrorMsg = 'Uw bevestigingspincode moet hetzelfde zijn.';
+		}	else {
+			const username = JSON.parse(localStorage.getItem('CURRENT_USER')).username;
+			const pinuser = { username: username, pin: pin };
+			localStorage.setItem('PIN_CODE_USER', JSON.stringify(pinuser));
+			console.log("TEST: " + JSON.stringify(pinuser));
+			this.router.navigateByUrl('');
 		}
 	}
 	form.reset();
