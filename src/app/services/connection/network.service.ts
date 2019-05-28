9b8b6a4e76aa73279de87cacf8d91444896334c9
Luuk van Deursen <ltadeurs@avans.nl>
2019-05-28 11:16:31 +0200

    Code cleaning

--- src/app/services/connection/network.service.ts
+++ src/app/services/connection/network.service.ts
@@ -1,7 +1,7 @@
-import {Injectable} from '@angular/core';
-import {BehaviorSubject, Observable} from 'rxjs';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import {environment} from '../../../environments/environment';
+import { environment } from '../../../environments/environment';
 
 export enum ConnectionStatus {
   Online,
