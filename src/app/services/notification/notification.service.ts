d242d69c0fd6e1d78cb5e4de25f2e0866c61c542
Anthony Tran <a.tran@student.avans.nl>
2019-05-29 21:01:02 +0200

    Subscribe to groups FCM

--- src/app/services/notification/notification.service.ts
+++ src/app/services/notification/notification.service.ts
@@ -1,70 +1,90 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {AlertController, Platform} from '@ionic/angular';
 import {ELocalNotificationTriggerUnit, LocalNotifications} from '@ionic-native/local-notifications/ngx';
 import {FCM} from '@ionic-native/fcm/ngx';
 import {Router} from '@angular/router';
+import {GroupService} from '../group/group.service';
+import {forEach} from '@angular-devkit/schematics';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class NotificationService {
 
-  history = [];
-  subscribeTopics = [];
+    history = [];
 
-  constructor(private plt: Platform, private localNotifications: LocalNotifications, private alertCtrl: AlertController, private fcm: FCM,
-              private router: Router) {
-    this.fcm.getToken().then(token => {
-      console.log(token);
-    });
-    this.fcm.onTokenRefresh().subscribe(token => {
-      console.log(token);
-    });
-    this.fcm.onNotification().subscribe(data => {
-      console.log(data);
-      if (data.wasTapped) {
-        console.log('Received in background');
-        this.router.navigateByUrl('/intakeMoment/' + data.id);
-      } else {
-        console.log('Received in foreground');
-        this.router.navigateByUrl('/intakeMoment/' + data.id);
-      }
-    });
-  }
+    constructor(private plt: Platform, private localNotifications: LocalNotifications, private alertCtrl: AlertController, private fcm: FCM,
+                private router: Router, private groupService: GroupService) {
+        // On load subscribe to FCM topics
+        this.subscribeToGroups(this.fcm);
+        // Subscribe on push notifications
+        this.fcm.onNotification().subscribe(data => {
+            if (data.wasTapped) {
+                // Do if app is in background/closed
+                this.router.navigateByUrl('/intakeMoment/' + data.id);
+            } else {
+                // Do if app is on foreground
+                this.router.navigateByUrl('/intakeMoment/' + data.id);
+            }
+        });
+    }
 
-  // Notification content
-  scheduleNotification(notification) {
-    this.localNotifications.schedule({
-      id: notification.id,
-      title: 'Melding',
-      text: 'Medicatie melding!',
-      data: '',
-      trigger: {at: new Date(notification.intake_start_time)},
-      foreground: true,
-      wakeup: true,
-      priority: 2,
-      vibrate: true,
-      launch: true,
-      silent: false
-    });
-  }
+    // Notification content
+    scheduleNotification(notification) {
+        this.localNotifications.schedule({
+            id: notification.id,
+            title: 'Melding',
+            text: 'Medicatie melding!',
+            data: '',
+            trigger: {at: new Date(notification.intake_start_time)},
+            foreground: true,
+            wakeup: true,
+            priority: 2,
+            vibrate: true,
+            launch: true,
+            silent: false
+        });
+    }
 
 
-  // When notification is clicked
-  showAlert(header, sub, msg) {
-    this.alertCtrl.create({
-      header: header,
-      subHeader: sub,
-      message: msg,
-      buttons: ['Ok'],
-    }).then(alert => alert.present());
-  }
+    // When notification is clicked
+    showAlert(header, sub, msg) {
+        this.alertCtrl.create({
+            header: header,
+            subHeader: sub,
+            message: msg,
+            buttons: ['Ok'],
+        }).then(alert => alert.present());
+    }
 
-  cancelAll() {
-    this.localNotifications.cancelAll();
-  }
+    cancelAll() {
+        this.localNotifications.cancelAll();
+    }
 
-  subscribeToTopic(groupId) {
-      this.fcm.subscribeToTopic(groupId);
-  }
+    subscribeToGroups(fcm: FCM) {
+        // Get old groups if exist
+        let subscribeTopics = JSON.parse(localStorage.getItem('subscribeTopics'));
+        // Unsubscribe from old group(s)
+        if (subscribeTopics) {
+            subscribeTopics.forEach(function (t) {
+                fcm.unsubscribeFromTopic('Group' + t);
+            });
+            localStorage.removeItem('subscribeTopics');
+        }
+        // Subscribe to topic (Group)
+        this.groupService.getGroupsOfDispenser().subscribe(
+            data => {
+                subscribeTopics = data;
+            },
+            error => {
+                console.log(error);
+            },
+            () => {
+                subscribeTopics = subscribeTopics.map(function (t) {
+                    fcm.subscribeToTopic('Group' + t.id);
+                    return t.id;
+                });
+                localStorage.setItem('subscribeTopics', JSON.stringify(subscribeTopics));
+            });
+    }
 }
