76a5352671b3643f57dc06a8e8666972e2bc7dcf
Anthony Tran <a.tran@student.avans.nl>
2019-06-19 16:20:39 +0200

    Remove start & end time, add range (x/x medicines)

--- src/app/pages/intake-moment-detail/intake-moment-detail.page.ts
+++ src/app/pages/intake-moment-detail/intake-moment-detail.page.ts
@@ -54,7 +54,8 @@ export class IntakeMomentDetailPage implements OnInit {
     }
 
     canSend(): boolean {
-        this.range = this.intakeMomentMedicines.filter(elem => elem.checked === true).length + '/' + this.intakeMomentMedicines.length;
+        this.range = this.intakeMomentMedicines.filter(elem => elem.completed_at !== null || elem.checked === true).length + '/'
+            + this.intakeMomentMedicines.length;
         return this.intakeMomentMedicines.filter(elem => elem.completed_at === null).length === 0;
     }
 
